78b30cd04658443b44f21827c71cd8683a3d0fd6
willianvmelo <willian.vieirademelo@gmail.com>
2021-07-19 21:08:09 -0300

    Criando tabelas contato e categoria.

--- agenda/settings.py
+++ agenda/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'agenda.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [BASE_DIR /'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
- os.path.join(BASE_DIR, 'templates/static')
+ BASE_DIR / 'templates/static'
 ]
 
 # Default primary key field type
