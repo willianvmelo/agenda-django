5c84bbc84ddd075c915faf9c38dd2c60a5ef0c78
willianvmelo <willian.vieirademelo@gmail.com>
2021-07-19 22:27:52 -0300

    Recuperando os dados dinamicamente do banco e criando a pagina de detalhamento dos contatos.

--- contatos/views.py
+++ contatos/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from .models import Contato
 
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.all() #Recupera dados do banco e armazena na variável
     return render(request,'contatos/index.html',{
         'contatos':contatos
+    })
+def ver_contato(request, contato_id):
+    contato = Contato.objects.get(id = contato_id) #Recupera dados do banco e armazena na variável
+    return render(request,'contatos/ver_contato.html',{
+        'contato':contato
     })
